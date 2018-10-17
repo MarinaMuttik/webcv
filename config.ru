128e434deabd83937566cd70e862615c7d85b923
MMuttik <mmuttik@live.co.uk>
2018-10-17 16:45:50 +0100

    "added config"

---
+++ config.ru
@@ -0,0 +1,6 @@
+require File.expand_path '../webcv.rb', __FILE__
+
+run Rack::URLMap.new({
+  "/" => Public,
+  "/workhistory" => Protected
+  })
--- webcv.rb
+++ webcv.rb
@@ -1,10 +1,21 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'slim'
 
+
+class Public < Sinatra::Base
 get '/' do
   slim :index
 end
+end
+
+class Protected < Sinatra::Base
+
+  use Rack::Auth::Basic, "Please enter login details" do |username, password|
+    username == "marina" && password == "workhis1"
+  end
 
 get '/workhistory' do
   slim :'workhistory/index'
+end
+
 end
