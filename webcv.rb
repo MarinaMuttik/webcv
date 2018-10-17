ba2dcef84872933c4d1e1483c1cc53d8cd1fb46e
MMuttik <mmuttik@live.co.uk>
2018-10-17 15:44:43 +0100

    "updated"

--- webcv.rb
+++ webcv.rb
@@ -1,25 +1,14 @@
+require 'sinatra'
 require 'sinatra/base'
 require 'slim'
 
-class Public < Sinatra::Base
+
 
 get '/' do
   slim :index
 end
 
-end
-
-class Protected < Sinatra::Base
-
-  use Rack::Auth::Basic, "Authentication Required!" do |username, password|
-    username == "marina" && password == "workhis1"
-  end
 
 get '/workhistory' do
   slim :'workhistory/index'
 end
-
-end
-
-Public.run!
-Protected.run!
