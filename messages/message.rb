b5c2e955eb4bd610471df738e7edd52b863cc747
MMuttik <mmuttik@live.co.uk>
2019-01-05 16:48:00 +0000

    added contact form and checked working with test messages

--- messages/message.rb
+++ messages/message.rb
@@ -1,2 +1,4 @@
 class Message < ActiveRecord::Base
+  validates :contact_name, presence: true
+  validates :contact_message, presence: true
 end
--- protected.rb
+++ protected.rb
@@ -19,4 +19,13 @@ class Protected < Sinatra::Base
     slim :'contact/index'
   end
 
+  post '/contact/submit' do
+    @message = Message.new(params[:message])
+  	if @message.save
+  		slim :'contact/success'
+  	else
+      slim :'contact/fail'
+  	end
+  end
+
 end
