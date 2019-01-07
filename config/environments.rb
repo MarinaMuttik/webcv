615bb2af2108bfae65aab0440427ec40882745ad
MMuttik <mmuttik@live.co.uk>
2019-01-07 00:08:59 +0000

    fixed environments.rb for development

--- config/environments.rb
+++ config/environments.rb
@@ -3,8 +3,8 @@ require 'sinatra/activerecord'
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
 
-configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+configure :production, :development do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/webcv_development')
 
  ActiveRecord::Base.establish_connection(
    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
